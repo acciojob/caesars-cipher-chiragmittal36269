36a3509f4cae58fc73e0a0afb077f0c8cc2555f5
Chirag Mittal <118048068+chiragmittal36269@users.noreply.github.com>
2023-06-11 16:40:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,51 +1,48 @@
 // Your Script here.
 
 const lookup = {
-  A: "N",
-  B: "O",
-  C: "P",
-  D: "Q",
-  E: "R",
-  F: "S",
-  G: "T",
-  H: "U",
-  I: "V",
-  J: "W",
-  K: "X",
-  L: "Y",
-  M: "Z",
-  N: "A",
-  O: "B",
-  P: "C",
-  Q: "D",
-  R: "E",
-  S: "F",
-  T: "G",
-  U: "H",
-  V: "I",
-  W: "J",
-  X: "K",
-  Y: "L",
-  Z: "M",
-  "?": "?",
-  ",": ",",
+	A: "N",
+	B: "O",
+	C: "P",
+	D: "Q",
+	E: "R",
+	F: "S",
+	G: "T",
+	H: "U",
+	I: "V",
+	J: "W",
+	K: "X",
+	L: "Y",
+	M: "Z",
+	N: "A",
+	O: "B",
+	P: "C",
+	Q: "D",
+	R: "E",
+	S: "F",
+	T: "G",
+	U: "H",
+	V: "I",
+	W: "J",
+	X: "K",
+	Y: "L",
+	Z: "M",
+	"?": "?",
+	",": ",",
 };
 
 function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
+	let decodedArr = []; // Your Result goes here
+	// Only change code below this line
 	for (let index = 0; index < encodedStr.length; index++) {
-		if(lookup[encodedStr[index]] == undefined)
-		{
-			decodedArr += encodedStr[index];
-		}
-		else
-		{
-			decodedArr += lookup[encodedStr[index]];
+		if (lookup[encodedStr[index]] == undefined) {
+			decodedArr.push(encodedStr[index]);
+		} else {
+			decodedArr.push(lookup[encodedStr[index]]);
 		}
 	}
 
-  return; //return decodedArr
+	return decodedArr; //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
